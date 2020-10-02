f6b2d04eb1f844a0d728ddbc7f44cdb3d28f417e
Swaraj Dash <swarajdash40@gmail.com>
2020-10-02 18:09:29 +0530

    Add web search

--- jarvis.py
+++ jarvis.py
@@ -2,6 +2,8 @@ import pyttsx3
 import speech_recognition as sr
 import datetime
 import wikipedia
+import webbrowser
+import os
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -56,4 +58,13 @@ if __name__ == "__main__":
             query = query.replace("wikipedia","")
             results = wikipedia.summary(query, sentences=2)
             speak("According to Wikipedia...")
+            print(results)
             speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open('youtube.com')
+        
+        elif 'open google' in query:
+            webbrowser.open('google.com')
+        
+        elif 'play music' in query:
